3c4697d4435c1b55c5f0f9dffb6110930556dba1
k4m1 <zachary.lyons.johnson@gmail.com>
2020-05-26 12:29:57 -0400

    init server and throw error if fail to bind and then do the same for non blocking

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,5 +7,6 @@ const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 32;
 
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
+    server.set_nonblocking(true).expect("Failed to intitalize non-blocking")
 }
