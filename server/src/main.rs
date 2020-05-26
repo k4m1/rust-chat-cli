d6eddeb681cbfd99a4cc786a12e25786c8435429
k4m1 <zachary.lyons.johnson@gmail.com>
2020-05-26 12:33:18 -0400

    vec for clients and init channel for strings

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,6 +7,15 @@ const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 32;
 
 fn main() {
+    // start the server and non blocking, throw errs if failure
     let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
-    server.set_nonblocking(true).expect("Failed to intitalize non-blocking")
+    server.set_nonblocking(true).expect("Failed to intitalize non-blocking");
+
+
+    // vector to store all of our clients
+    let mut clients = vec![];
+    // create a channel and tell him 'hey bud im going to be sending some strings here that cool?'
+    let (tx, rx) = mspec ::channel::<String>();
+    
+
 }
