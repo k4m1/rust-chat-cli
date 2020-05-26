83eec52034ad3418a59f8d49980831896529de33
k4m1 <zachary.lyons.johnson@gmail.com>
2020-05-26 12:38:34 -0400

    added clone tx and clone client + fail msg

--- server/src/main.rs
+++ server/src/main.rs
@@ -16,6 +16,15 @@ fn main() {
     let mut clients = vec![];
     // create a channel and tell him 'hey bud im going to be sending some strings here that cool?'
     let (tx, rx) = mspec ::channel::<String>();
-    
+    // if let to desconstruct the result from server.accept (what allows us to accept connections)
+    loop {
+        if let ok((mut socket, addr)) = server.accept() {
+            println!("client {} connected", addr);
 
+            // clone the transmitter
+            let tx = tx.clone();
+            // copy client to be pushed into thread
+            clients.push(socket.try_clone().expect("failed to clone client"));
+        }
+    }
 }
