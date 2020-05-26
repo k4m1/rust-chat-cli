047e607246c7e2e2da8c8ee2afeb2ab050934956
k4m1 <zachary.lyons.johnson@gmail.com>
2020-05-26 13:54:19 -0400

    added client non blicking and create the channel for strings

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,7 +4,19 @@ use std::sync::mspc::{Self, TryRecvError};
 use std::thread;
 use std::time::Duration;
 
+const LOCAL: &str = "127.0.0.1:6000"
+const MSG_SIZE: usize = 32; 
 
 fn main() {
-    println!("Hello, world!");
+    //create mutable Tcpstream called client and if we fail to connect throw an err
+    // do the same for non blocking
+    let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
+    client.set_nonblocking(true).expect("failed to initate non-blocking");
+
+    // like in server we make a channel for strings
+    let (tx, rx) = mspc::channel::<String>();
+
+
+
+
 }
